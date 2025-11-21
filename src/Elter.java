0c16972d034ddf1bfca941b7690e0c3d854e45a9
Kancho_Novakov <kancho.novakov@stud.uni-due.de>
2025-11-21 20:06:23 +0100

    Elter.java Korrigiert

--- src/Elter.java
+++ src/Elter.java
@@ -1,2 +1,45 @@
+import java.util.List;
+import java.util.ArrayList;
 public class Elter extends Nutzer { //Datei lesen.
+    //Attribute
+    private List<Schüler> kinder;
+
+    //Konstruktor
+    public Elter(int nutzerId, String vorname, String nachname, String benutzername,
+                 String passwort, String erkennungscode) {
+        super(nutzerId, vorname, nachname, benutzername, passwort, erkennungscode);
+        this.kinder = new ArrayList<>();
+    }
+
+    //Funktionen
+
+    //Verbindung zwischen Lehrer, setElter
+    public void addKind(Schüler kind) {
+        if (!this.kinder.contains(kind)) {
+            this.kinder.add(kind);
+            System.out.println(getVollerName() + " wurde mit Kind " + kind.getVollerName() + " verbunden.");
+        } else {
+            System.out.println(kind.getVollerName() + " ist bereits verknüpft.");
+        }
+    }
+
+    /*
+    public void zeigeKinderAufgaben(Schüler kind) {
+        if (this.kinder.contains(kind)) {
+            System.out.println("Elter " + getVollerName() + " zeigt Aufgaben von " + kind.getVollerName());
+        } else {
+            System.out.println(kind.getVollerName() + " ist kein verknüpftes Kind.");
+        }
+    }*/ //Idee*
+
+    //Getter und Setter Methoden
+    public List<Schüler> getKinder() {
+        return kinder;
+    }
+
+    //toString()
+    @Override
+    public String toString() {
+        return super.toString() + " [Rolle: Elter, Anzahl Kinder: " + kinder.size() + "]";
+    }
 }
