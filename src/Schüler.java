221ac108b257e6c5dd95893c6fa68a03679bdca2
Kancho_Novakov <kancho.novakov@stud.uni-due.de>
2025-11-21 20:15:39 +0100

    Schüler.java Basis

--- src/Schüler.java
+++ src/Schüler.java
@@ -1,2 +1,20 @@
+import java.util.List;
+import java.util.ArrayList;
 public class Schüler extends Nutzer { //Datei lesen und senden
-}
+    //Attributen
+    private Elter elter; //Verbindung mit dem Elter
+    private List<Aufgabe> abgegebeneAufgaben; //Aufgaben
+
+    //Konstruktor
+    public Schüler(int nutzerId, String vorname, String nachname, String benutzername,
+                   String passwort, String erkennungscode) {
+        super(nutzerId, vorname, nachname, benutzername, passwort, erkennungscode); //super*
+        this.abgegebeneAufgaben = new ArrayList<>();
+        this.elter = null;
+    }
+
+    public void sendeAufgabe(Aufgabe aufgabe) {
+        this.abgegebeneAufgaben.add(aufgabe);
+        System.out.println(getVollerName() + " hat Aufgabe '" + aufgabe.getTitel() + "' abgegeben.");
+    }
+}//Basis fr Schüler.
