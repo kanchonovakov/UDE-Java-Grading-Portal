6f0290538c193a45aad9c9db312724cbc44a4074
Kancho_Novakov <kancho.novakov@stud.uni-due.de>
2025-11-21 19:32:33 +0100

    TODO Liste

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,38 @@
 /*
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
     IO.println(String.format("Hello and welcome!"));
 
     for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+      IO.println("i = " + i);
     }
 }
 */
+//Kommentare sind wichtig*
+//Sinvolle Benennung
+
+//TODO Atrubuten
+//TODO Klassenkonstruktoren
+//TODO Getter- und Setter-Methoden
+//TODO Funktionen der Klassen
+//TODO Funktionsprototypen -Methodenname, Übergabeparameter und Rückgabetyp, nur ohne die eigentliche Funktionalität
+//TODO Zugriffsmodifikatoren (public, protected, no-modifier, private) ---private als Standard---
+//TODO Kommentieren Sie bei jeder Methode, die nicht private ist, warum sie keinen Modifier hat oder public protected sein muss.
+//TODO Überlegen Sie, wie Ihre bestehenden Klassen sinnvoll miteinander verknüpft werden können, und implementieren Sie diese Beziehungen.
+//TODO Vererbung
+//TODO Schnittstelle (Interface) Welche Klassen sind betroffen und welche Informationen werden übergeben
+
+//TODO toString()
+//Überschreiben Sie die Methode so, dass sie eine aussagekräftige Beschreibung des Objekts
+//liefert. Geben Sie alle relevanten Attribute in lesbarer Form aus.
+
+//TODO equals(Object obj)
+//Überschreiben Sie die Methode so, dass zwei Objekte als gleich gelten, wenn alle relevanten
+//Attribute übereinstimmen. Achten Sie auf: Typprüfung (instanceof), Nullprüfung,
+//Vergleich der Attribute mit equals().
+
+//TODO hashCode()
+//Implementieren Sie die Methoden so, dass sie einheitlich und konsistent mit equals() sind.
+//Orientieren Sie sich dabei an den Beispielen aus den Vorlesungsfolien.
 
 void main(){
     IO.println("Projektidee 1: Hausaufgaben abgeben und verwalten.");
